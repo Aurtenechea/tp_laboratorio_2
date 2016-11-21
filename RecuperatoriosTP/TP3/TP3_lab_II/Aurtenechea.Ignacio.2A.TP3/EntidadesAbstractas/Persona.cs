32d8955baafe8b6521f9bab4f41f45bad18eb7eb
ignacio aurtenechea <ignacio.aurtenechea@gmail.com>
2016-11-21 19:31:41 -0300

    tp3 en carpeta Recuperatorio

---
+++ RecuperatoriosTP/TP3/TP3_lab_II/Aurtenechea.Ignacio.2A.TP3/EntidadesAbstractas/Persona.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Excepciones;
+
+namespace EntidadesAbstractas
+{
+    [Serializable]
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        { 
+            Argentino,
+            Extranjero
+        }
+
+        #region atributos
+
+        private string _nombre;
+        private string _apellido;
+        private ENacionalidad _nacionalidad;
+        private int _dni;
+
+        #endregion
+
+        #region propiedades
+
+        /// <summary>
+        /// Devuelve el nombre de la persona o bien guarda el nombre de la persona analizando previamente si el string contiene caracteres
+        /// especiales. Si contiene esos caracteres no guardara nada en nombre.
+        /// </summary>
+        public string Nombre
+        {
+            get 
+            {
+                return this._nombre;
+            }
+            set 
+            {
+                if (ValidarNombreApellido(value))
+                {
+                    this._nombre = value;
+                }
+                else 
+                {
+                    throw new Exception("Nombre invalido.");
+                }
+            }
+        }
+       
+        /// <summary>
+        /// Devuelve el apellido de la persona o bien guarda el apellido de la persona analizando previamente si el string contiene caracteres
+        /// especiales. Si contiene esos caracteres no guardara nada en apellido.
+        /// </summary>
+        public string Apellido
+        {
+            get
+            {
+                return this._apellido;
+            }
+            set
+            {
+                if (ValidarNombreApellido(value))
+                {
+                    this._apellido = value;
+                }
+                else
+                {
+                    throw new Exception("Apellido invalido.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retornara o guardara el dni de la persona. Al guardarlo previamente sera validado segun su nacionalidad.
+        /// </summary>
+        public int DNI
+        {
+            get 
+            {
+                return this._dni;
+            }
+            set 
+            {
+                this._dni = ValidarDni(this.Nacionalidad ,value);
+            }
+        }
+
+        /// <summary>
+        /// Retornara o guardara la nacionalidad de la persona.
+        /// </summary>
+        public ENacionalidad Nacionalidad
+        {
+            get 
+            {
+                return this._nacionalidad;
+            }
+            set 
+            {
+                this._nacionalidad = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Guarda el dni de la persona pasado como string. Antes de guardarlo lo valida segun su nacionalidad.
+        /// </summary>
+        public string StringToDNI
+        {
+            set 
+            {
+                this._dni = ValidarDni(this.Nacionalidad, value);
+            }
+        }
+
+        #endregion
+
+        #region constructores
+
+        
+        #region para serializar
+        /// <summary>
+        /// constructor por defecto usado para serializar.
+        /// </summary>
+        public Persona() { }
+        #endregion
+       
+
+        /// <summary>
+        /// Inicializa los campos nombre, apellido y nacionalidad al crear una persona.
+        /// </summary>
+        /// <param name="nombre">Nombre de la persona.</param>
+        /// <param name="apellido">Apellido de la persona.</param>
+        /// <param name="nacionalidad">Nacionalidad de la persona.</param>
+        protected Persona(string nombre, string apellido, ENacionalidad nacionalidad) 
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
+
+        /// <summary>
+        /// Inicializa los campos nombre, apellido, dni (como int) y nacionalidad al crear una persona.
+        /// </summary>
+        /// <param name="nombre">Nombre de la persona.</param>
+        /// <param name="apellido">Apellido de la persona.</param>
+        /// <param name="dni">Dni de la persona como int.</param>
+        /// <param name="nacionalidad">Nacionalidad de la persona.</param>
+        protected Persona(string nombre, string apellido,int dni, ENacionalidad nacionalidad)
+            :this(nombre,apellido,nacionalidad)
+        {
+            this.DNI = dni;
+        }
+
+        /// <summary>
+        /// Inicializa los campos nombre, apellido, dni (como string) y nacionalidad al crear una persona.
+        /// </summary>
+        /// <param name="nombre">Nombre de la persona.</param>
+        /// <param name="apellido">Apellido de la persona.</param>
+        /// <param name="dni">Dni de la persona como string.</param>
+        /// <param name="nacionalidad">Nacionalidad de la persona.</param>
+        protected Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : this(nombre, apellido, nacionalidad)
+        {
+            this.StringToDNI = dni;
+        }
+
+        #endregion
+
+        #region metodos
+
+        /// <summary>
+        /// Devuelve un string con los datos de la persona.
+        /// </summary>
+        /// <returns>string con los datos de la persona.</returns>
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("NOMBRE COMPLETO: " + this.Apellido + ", " + this.Nombre);
+            sb.AppendLine("NACIONALIDAD: " + this.Nacionalidad);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Valida el dni de una persona segun su nacionalidad.
+        /// </summary>
+        /// <param name="nacionalidad">Nacionalidad de la persona</param>
+        /// <param name="dato">Dni de la persona como int</param>
+        /// <returns>
+        /// Dni. Si dato es un entero entre 1 y 89999999 y la nacionalidad es argentino, o si dni esta entre 90000000 y 99999999 y 
+        /// la nacionalidad es extranjero.
+        /// 0. Si no es ninguno de los casos anteriores. 
+        /// </returns>
+        private static int ValidarDni(ENacionalidad nacionalidad, int dato) 
+        {
+            if (nacionalidad == ENacionalidad.Argentino && (dato > 89999999 || dato < 1))
+                throw new NacionalidadInvalidaException();
+            if (nacionalidad == ENacionalidad.Extranjero && (dato > 99999999 || dato < 90000000))
+                throw new NacionalidadInvalidaException();
+            return dato;
+
+            /*
+             * sigo pensando que esta mal. Si a uno le pasan una persona en la que no se corresponden los datos
+             * no se tiene como determinar si lo que esta mal es el dni o su nacionalidad.
+             * Y no esta explicitado en el PDF. Solo que el main captura NacionalidadInvalidaException,
+             * con lo cual para mi también esta mal.
+             * Por ej podrian poner una nacionalidad extranjero. Al ingresar el dni se comen un numero y ahi sale la excepcion 
+             * nacionalidad invalida, cosa que no es cierta. La nacionalidad es correcta, es el dni el que es invalido para esa nacionalidad.
+             * Yo crearia una excepcion: NacionalidadDniNoCorrespondientesException()
+             */
+        }
+
+
+        /// <summary>
+        /// Valida el dni de una persona segun su nacionalidad.
+        /// </summary>
+        /// <param name="nacionalidad">Nacionalidad de la persona</param>
+        /// <param name="dato">Dni de la persona como int</param>
+        /// <returns>
+        /// Dni. Si dato es un entero entre 1 y 89999999 y la nacionalidad es argentino, o si dni esta entre 90000000 y 99999999 y 
+        /// la nacionalidad es extranjero.
+        /// 0. Si no es ninguno de los casos anteriores, o si el string pasado no es un numero.
+        /// </returns>
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int numero;
+            if (!int.TryParse(dato, out numero))
+                throw new DniInvalidoException();
+            return ValidarDni(nacionalidad, numero);
+        }
+
+        private bool ValidarNombreApellido(string value) 
+        {
+            Regex RgxUrl = new Regex("[^A-Z a-záéíóú]");
+            bool containsSpecialCharacters = RgxUrl.IsMatch(value);
+            if (!containsSpecialCharacters)
+                return true;
+            return false;
+        }
+
+
+
+        #endregion
+
+    }
+}
---
+++ RecuperatoriosTP/TP3/TP3_lab_II/Aurtenechea.Ignacio.2A.TP3/EntidadesAbstractas/PersonaGimnasio.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesAbstractas
+{
+    [Serializable]
+    public abstract class PersonaGimnasio :Persona
+    {
+        #region atributos
+
+        private int _identificador;
+
+        #endregion
+
+        #region para serializar
+
+        public int ID 
+        {
+            get { return this._identificador; }
+            set { this._identificador = value; }
+        }
+
+        public PersonaGimnasio() { }
+
+        #endregion
+
+        #region constructores
+
+        /// <summary>
+        /// inicializa los campos de PersonaGimnasio
+        /// </summary>
+        /// <param name="id">id de la PersonaGimnasio</param>
+        /// <param name="nombre">nombre de la PersonaGimnasio</param>
+        /// <param name="apellido">apellido de la PersonaGimnasio</param>
+        /// <param name="dni">dni de la PersonaGimnasio</param>
+        /// <param name="nacionalidad">nacionalidad de la PersonaGimnasio</param>
+        public PersonaGimnasio(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad) 
+            :base( nombre, apellido, dni,  nacionalidad)
+        {
+            this._identificador = id;
+        }
+
+        #endregion
+
+        #region metodos
+
+        /// <summary>
+        /// Devuelve si dos PersonaGimnasio son del mismo tipo.
+        /// </summary>
+        /// <param name="obj">Objeto con el cual se quiere comparar this.</param>
+        /// <returns>
+        /// true si son del mismo tipo.
+        /// false si no lo son.
+        /// </returns>
+        public bool Equals(object obj)
+        {
+            // No aclara que pide.
+            // si pide que haga lo mismo que el == entonces esta mal.
+            // para poder darle un uso puse que retorne si son del mismo tipo.
+            return (obj is PersonaGimnasio);
+            
+        }
+
+        /// <summary>
+        /// Retorna un string con los datos de una persona gimnasio.
+        /// </summary>
+        /// <returns> string con los datos de una persona gimnasio.</returns>
+        protected virtual string MostrarDatos()
+        {
+            return base.ToString() + "\nCARNET NUMERO: " + this._identificador.ToString();
+        }
+
+        protected abstract string ParticiparEnClase();
+
+        /// <summary>
+        /// Dos PersonaGimnasio son iguales si son del mismo tipo y su id o DNI son iguales.
+        /// </summary>
+        /// <param name="pg1">PersonaGimnasio a comparar.</param>
+        /// <param name="pg2">PersonaGimnasio a comparar.</param>
+        /// <returns>
+        /// true si son iguales.
+        /// false si no lo son.
+        /// </returns>
+        public static bool operator == (PersonaGimnasio pg1, PersonaGimnasio pg2)
+        {
+            if ( pg1.Equals(pg2) && (pg1.DNI == pg2.DNI || pg1._identificador == pg2._identificador))
+                return true;
+                return false;
+        }
+
+        /// <summary>
+        /// Dos PersonaGimnasio son iguales si son del mismo tipo y su id o DNI son iguales.
+        /// </summary>
+        /// <param name="pg1">PersonaGimnasio a comparar.</param>
+        /// <param name="pg2">PersonaGimnasio a comparar.</param>
+        /// <returns>
+        /// true si son distintas.
+        /// false si no lo son.
+        /// </returns>
+        public static bool operator !=(PersonaGimnasio pg1, PersonaGimnasio pg2)
+        {
+            return !(pg1 == pg2);
+        }
+
+        #endregion
+        
+
+    }
+}
